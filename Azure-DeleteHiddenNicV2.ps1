f33293418920794d90942f21b11bf4f17276da0a
Carlos Mayol (MSFT) <carlosmayol@users.noreply.github.com>
2021-07-27 09:26:37 -0400

    Create Azure-DeleteHiddenNicV2.ps1

---
+++ Azure-DeleteHiddenNicV2.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\DeviceManagement.psd1 -Verbose
+$hiddenHypVNics = Get-Device -ControlOptions DIGCF_ALLCLASSES | 
+    Sort-Object -Property Name | 
+        Where-Object { ($_.IsPresent -eq $false) -and ($_.Name -like "Microsoft Hyper-V Network Adapter*") }
+ 
+$hiddenHypVNics | ForEach-Object {
+    Write-Verbose "Removing ""@$($_.InstanceId.Replace("`0", ''))"""
+    .\devcon.exe remove """@$($_.InstanceId.Replace("`0", ''))"""
+}
